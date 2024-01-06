f2900d323da707ff259536720b9b5e077f0d92dc
AbishakeSrithar <sritharabishake@gmail.com>
2024-01-06 18:01:49 +0000

    10-11: Pawn I and Pawn II

--- src/main/java/com/chessengine/board/BoardUtils.java
+++ src/main/java/com/chessengine/board/BoardUtils.java
@@ -5,6 +5,9 @@ public class BoardUtils {
     public static final boolean[] SECOND_COLUMN = initColumn(1);
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
+    public static final boolean[] SECOND_ROW = initRow(2);
+    public static final boolean[] SEVENTH_ROW = initRow(7);
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
@@ -19,6 +22,15 @@ public class BoardUtils {
             columnNumber += 8;
         } while(columnNumber < 64);
         return column;
+    }
+
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[64];
+        final int firstOfRow = (rowNumber - 1) * 8;
+        for (int i = 0; i < NUM_TILES_PER_ROW; i++) {
+            row[firstOfRow + i] = true;
+        }
+        return row;
     }
 
     public static boolean isValidTileCoordinate(final int coordinate) {
---
+++ src/main/java/com/chessengine/pieces/Pawn.java
@@ -0,0 +1,52 @@
+package com.chessengine.pieces;
+
+import com.chessengine.Alliance;
+import com.chessengine.board.Board;
+import com.chessengine.board.BoardUtils;
+import com.chessengine.board.Move;
+import com.chessengine.board.Move.MajorMove;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Pawn extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {8, 16};
+    Pawn(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+
+            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+
+            if (!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                continue;
+            }
+
+            if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                // TODO: Needs updating
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+            } else {
+                if (currentCandidateOffset == 16 && this.isFirstMove()) {
+                    if ((BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack()) || (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite())) {
+                        final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+                        if (!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                        !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                            legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return legalMoves;
+    }
+}
