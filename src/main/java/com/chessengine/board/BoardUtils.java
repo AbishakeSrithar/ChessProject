829a2bf5094473aa86c3f56f8f34bb64da8e8ad2
AbishakeSrithar <sritharabishake@gmail.com>
2024-01-07 14:18:00 +0000

    13: King

--- src/main/java/com/chessengine/board/BoardUtils.java
+++ src/main/java/com/chessengine/board/BoardUtils.java
@@ -5,9 +5,11 @@ public class BoardUtils {
     public static final boolean[] SECOND_COLUMN = initColumn(1);
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+    public static final boolean[] FIRST_ROW = initRow(1);
 
     public static final boolean[] SECOND_ROW = initRow(2);
     public static final boolean[] SEVENTH_ROW = initRow(7);
+    public static final boolean[] EIGHTH_ROW = initRow(8);
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
---
+++ src/main/java/com/chessengine/pieces/King.java
@@ -0,0 +1,72 @@
+package com.chessengine.pieces;
+
+import com.chessengine.Alliance;
+import com.chessengine.board.Board;
+import com.chessengine.board.BoardUtils;
+import com.chessengine.board.Move;
+import com.chessengine.board.Move.AttackMove;
+import com.chessengine.board.Move.MajorMove;
+import com.chessengine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {-9, -8, -7, -1, 1, 7, 8, 9};
+
+    King(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
+
+            int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if (isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isEightColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isFirstRowExclusion(this.piecePosition, currentCandidateOffset) || // Not in video but seems like it should be here?
+                        isEightRowExclusion(this.piecePosition, currentCandidateOffset)) { // Not in video but seems like it should be here?
+                    continue;
+                }
+
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                if (!candidateDestinationTile.isTileOccupied()) {
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                } else {
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                    if (this.pieceAlliance != pieceAlliance) {
+                        legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffSet) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffSet == -9 || candidateOffSet == -1 || candidateOffSet == 7);
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffSet) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffSet == -7 || candidateOffSet == 1 || candidateOffSet == 9);
+    }
+
+    private static boolean isFirstRowExclusion(final int currentPosition, final int candidateOffSet) { // Not in video but seems like it should be here?
+        return BoardUtils.FIRST_ROW[currentPosition] && (candidateOffSet == -9 || candidateOffSet == -8 || candidateOffSet == -7);
+    }
+
+    private static boolean isEightRowExclusion(final int currentPosition, final int candidateOffSet) { // Not in video but seems like it should be here?
+        return BoardUtils.EIGHTH_ROW[currentPosition] && (candidateOffSet == 7 || candidateOffSet == 8 || candidateOffSet == 9);
+    }
+}
