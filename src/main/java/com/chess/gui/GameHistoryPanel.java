2b6b256ab963f60b7b29caeba4a81d9242b1254a
AbishakeSrithar <sritharabishake@gmail.com>
2024-02-20 17:33:17 +0000

    39: GUI XI

---
+++ src/main/java/com/chess/gui/GameHistoryPanel.java
@@ -0,0 +1,112 @@
+package com.chess.gui;
+
+import com.chess.engine.board.Board;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class GameHistoryPanel extends JPanel {
+    private final DataModel model;
+    private final JScrollPane scrollPane;
+    private static final Dimension HISTORY_PANEL_DIMENSION = new Dimension(100, 400);
+
+    GameHistoryPanel() {
+        this.setLayout(new BorderLayout());
+        this.model = new DataModel();
+        final JTable table = new JTable(model);
+        table.setRowHeight(15);
+        this.scrollPane = new JScrollPane(table);
+        scrollPane.setColumnHeaderView(table.getTableHeader());
+        scrollPane.setPreferredSize(HISTORY_PANEL_DIMENSION);
+        this.add(scrollPane, BorderLayout.CENTER);
+        this.setVisible(true);
+    }
+
+    void redo(final Board board, final Table.MoveLog moveLog) {
+
+    }
+
+    private static class DataModel extends DefaultTableModel {
+        private final java.util.List<Row> values;
+        private static final String[] NAMES = {"White", "Black"};
+
+        DataModel() {
+            this.values = new ArrayList<>();
+        }
+
+        public void clear() {
+            this.values.clear();
+            setRowCount(0);
+        }
+
+        @Override
+        public int getRowCount() {
+            if (this.values == null) {
+                return 0;
+            }
+            return this.values.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return NAMES.length;
+        }
+
+        @Override
+        public Object getValueAt(final int row, final int column) {
+            final Row currentRow = this.values.get(row);
+            if (column == 0) {
+                return currentRow.getWhiteMove();
+            } else if (column == 1) {
+                return currentRow.getBlackMove();
+            }
+            return null;
+        }
+
+        @Override
+        public  void setValueAt(final Object aValue, final int row, final int column) {
+            final Row currentRow;
+            if (this.values.size() <= row) {
+                currentRow = new Row();
+                this.values.add(currentRow);
+            } else {
+                currentRow = this.values.get(row);
+            }
+            if (column == 0) {
+                currentRow.setWhiteMove((String) aValue);
+            } else if (column == 1) {
+                currentRow.setBlackMove((String) aValue);
+            }
+            // Why would this be only for col 1? Moved here but could be wrong
+            fireTableCellUpdated(row, column);
+        }
+    }
+
+    private static class Row {
+        private String whiteMove;
+        private String blackMove;
+
+        Row() {
+
+        }
+
+        public String getWhiteMove() {
+            return this.whiteMove;
+        }
+
+        public String getBlackMove() {
+            return this.blackMove;
+        }
+
+        public void setWhiteMove(final String move) {
+            this.whiteMove = move;
+        }
+
+        public void setBlackMove(final String move) {
+            this.whiteMove = move;
+        }
+    }
+
+}
